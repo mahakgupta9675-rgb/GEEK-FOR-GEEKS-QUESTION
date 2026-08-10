71fcaaf897ef114d6c3882fc2657defc433f5104
Mahak Gupta <mahakgupta9675@gmail.com>
2026-08-11 00:29:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNum(vector<int>& arr) {
+        int n = arr.size() + 1;
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            ans ^= i;
+        }
+
+        for (int x : arr) {
+            ans ^= x;
+        }
+
+        return ans;
+    }
+};
